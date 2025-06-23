3cac8adce1b4cf21de2e3805f86e80be0709b4e9
LiMaCor <julian.itsa8@gmail.com>
2025-06-23 21:25:26 +0200

    updated: added raw values to "Status" enum and a method to retrieve their String value

---
+++ src/model/Status.java
@@ -0,0 +1,24 @@
+package model;
+
+public enum Status {
+    TODO("ToDo"),
+    IN_PROGRESS("In progress"),
+    DONE("Done");
+
+    private final String value;
+
+    Status(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String toString() {
+        return "Status{" +
+                "value='" + value + '\'' +
+                '}';
+    }
+}
