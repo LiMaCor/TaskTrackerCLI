be54f77f27648f03b3699ad4dfb129cf52001c74
LiMaCor <julian.itsa8@gmail.com>
2025-06-23 21:43:35 +0200

    updated: removed unused imports

--- src/utils/Functions.java
+++ src/utils/Functions.java
@@ -1,10 +1,8 @@
 package utils;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 
 public class Functions {
 
