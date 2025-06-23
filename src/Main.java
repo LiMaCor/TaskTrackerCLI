4fe8d060f23b1ce6fa5bf81c001e029594c08046
LiMaCor <julian.itsa8@gmail.com>
2025-06-23 22:52:26 +0200

    updated: initialization of "TaskHandler" and handling the operation passed as arguments

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,22 @@ import java.io.IOException;
 
 import static utils.Functions.*;
 import handlers.InputHandler;
+import handlers.TaskHandler;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        TaskHandler taskHandler;
+
         try {
             if (!validateInitialFileStructure()) {
-                createTasksFile();
+                taskHandler = new TaskHandler(createTasksFile());
+            } else {
+                taskHandler = new TaskHandler(getTaskFilePath());
             }
 
             InputHandler inputHandler = new InputHandler(args);
+
+            inputHandler.handleOperations(taskHandler);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
