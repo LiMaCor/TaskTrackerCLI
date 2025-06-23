c9aecfdfc4174c838a448dbf7f553645c9ac51f1
LiMaCor <julian.itsa8@gmail.com>
2025-06-23 22:13:24 +0200

    updated: added methods to read and save to "tasks.json" file

---
+++ src/handlers/TaskHandler.java
@@ -0,0 +1,72 @@
+package handlers;
+
+import model.Task;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskHandler {
+    protected final Path FILE_PATH;
+    protected List<Task> tasks;
+
+    public TaskHandler(Path pathToTaskFile) {
+        this.FILE_PATH = pathToTaskFile;
+        this.tasks = loadTasksFromFile();
+    }
+
+    public void saveTasksToFile() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("[\n");
+
+        for (int i = 0; i < tasks.size(); i++) {
+            sb.append(tasks.get(i).toJson());
+
+            if (i < tasks.size() - 1) {
+                sb.append(",\n");
+            }
+        }
+
+        sb.append("\n]");
+
+        String jsonContent = sb.toString();
+
+        try {
+            Files.writeString(this.FILE_PATH, jsonContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Task> loadTasksFromFile() {
+        List<Task> loadedTasks = new ArrayList<>();
+
+        try {
+            String jsonContent = Files.readString(this.FILE_PATH);
+
+            if (jsonContent.isEmpty()) {
+                return new ArrayList<>();
+            }
+
+            String[] taskList = jsonContent.replace("[", "")
+                    .replace("]", "")
+                    .split("},");
+
+            for (String taskJson : taskList) {
+                if (!taskJson.endsWith("}")) {
+                    taskJson = taskJson + "}";
+                    loadedTasks.add(Task.taskFromJson(taskJson));
+                } else {
+                    loadedTasks.add(Task.taskFromJson(taskJson));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return loadedTasks;
+    }
+}
